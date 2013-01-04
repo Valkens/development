8e87c1f0559f22cec50adaa62cb956db7edc0958
duclh_younet <lehoangduc.vt@gmail.com>
2013-01-04 09:30:03 +0700

    update

--- s3299686/assignment2/admin/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username']) == 'admin') {
-    header('location: ../login.php');
-} else {
-    header('location: dashboard');
-}
-exit();
--- s3299686/assignment2/pdo.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql: host=localhost; dbname=s3299686", "s3299686", "qwerty1234",
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
